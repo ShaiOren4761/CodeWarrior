62cc8807b524bd72b2ed8ef7925c46099f8db3cf
ShaiOren4761 <shaioren100@gmail.com>
2023-04-29 00:22:47 +0300

    Second attempt at Simplified Array, redoing the Check_Prime function to a more simple way after learning more about prime numbers, and finding an impossible test that has hindered my progress. Left a comment on the kata, hoping for the best.

--- Simplified Array.py
+++ Simplified Array.py
@@ -1,35 +1,42 @@
 # https://www.codewars.com/kata/59015f8cc842a3e7f10000a4
 
+#
+# def Check_Prime(n):
+#     if n == 1:
+#         return False
+#     elif n == 2:
+#         return True
+#     else:
+#         return all(n % i for i in (2, n+1)) # This Doesn't work!! Why???????
+
 
 def Check_Prime(n):
-    if n == 1:
+    n = abs(n)
+    if n == 1 or n == 0:
         return False
-    elif n == 2:
-        return True
-    else:
-        return all(n % i for i in (2, n+1))
+    for i in range(2, int(n ** 0.5)+1):
+        if not n % i:
+            return False
+    return True
+
 
 def simplified_array(a):
-    flag = ''  # p for prime, np for non prime
     ret = []
     temp = []
+    flag = 'p' if a and Check_Prime(a[0]) else 'np'  # p for prime, np for non prime
 
     for i in a:
         bflag = Check_Prime(i)
 
         if flag == 'p' and not bflag:
-            ret.append(sum(temp))
-            temp.clear()
             flag = 'np'
-        elif flag == 'np' and bflag:
             ret.append(sum(temp))
             temp.clear()
+        elif flag == 'np' and bflag:
             flag = 'p'
-        elif not flag:
-            if bflag:
-                flag = 'p'
-            else:
-                flag = 'np'
+            ret.append(sum(temp))
+            temp.clear()
+
         temp.append(i)
 
     bflag = Check_Prime(i)
@@ -45,9 +52,8 @@ def simplified_array(a):
     return ret if ret == a else simplified_array(ret)
 
 
-print(simplified_array([21, 5]))
-# Something is seriously wrong with Check_Prime, as the function claims that 21 is a prime number. It is not.
-# Despite using all() which only returns True if ALL returned true, it still manages to return true
-# despite 21%7 being 0!!!!
-# At this point of testing I have realized that the code is rebelling and the universe is now AI.
-
+print(simplified_array([-3, 4, 5, 2, 0, -10]))
+# Test [-3, 4, 5, 2, 0, -10] --> [(-3 + 4), (5 + 2), (0 + -10)] --> [1, 7, -10]
+# -3 and 4 are not meant to be sum'd
+# Cannot continue to solution as impossible test was put as a hurdle in the way..
+# Or I missed something. Either way I'll return here once someone responded to my comment on the Kata.
