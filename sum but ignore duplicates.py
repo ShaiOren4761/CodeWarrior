3e31a1ad656ddc9dfae83a8b98b70ba392dcab83
ShaiOren4761 <shaioren100@gmail.com>
2023-06-15 07:54:45 +0300

    added link to exercise. Poor documentation by me.

--- sum but ignore duplicates.py
+++ sum but ignore duplicates.py
@@ -1,14 +1,16 @@
+# https://www.codewars.com/kata/5993fb6c4f5d9f770c0000f2
+
+def sum_no_duplicates(lst):
+    return sum([x for x in lst if lst.count(x) == 1])
 
 
-def sum_no_duplicates(l):
-    return sum([x for x in l if l.count(x) == 1])
+def sum_no_duplicates_ultimate(lst):
+    return sum([x for x in set(lst) if lst.count(x) == 1])
 
-def sum_no_duplicates_ultimate(l):
-    return sum([x for x in set(l) if l.count(x) == 1])
 
-l = [1, 1, 111]
+test = [1, 1, 111]
 
-print(sum_no_duplicates(l))
-print(sum_no_duplicates_ultimate(l))
+print(sum_no_duplicates(test))
+print(sum_no_duplicates_ultimate(test))
 
 
