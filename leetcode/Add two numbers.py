a87cce282426864e9e35cfae3090acbc4401a84d
ShaiOren4761 <shaioren100@gmail.com>
2023-08-26 15:02:43 +0300

    add two numbers progression

--- leetcode/Add two numbers.py
+++ leetcode/Add two numbers.py
@@ -17,12 +17,16 @@ class Solution(object):
             save_num = l2.val % 10
             remember_num = save_num // 10
             l2 = l2.next
+            if l2:
+                l2.val = l2.val + remember_num
             return ListNode(save_num, self.addTwoNumbers(l1, l2))
 
         elif not l2:  # Number on l1, save it and continue.
             save_num = l1.val % 10
             remember_num = save_num // 10
             l1 = l1.next
+            if l1:
+                l1.val = l1.val + remember_num
             return ListNode(save_num, self.addTwoNumbers(l1, l2))
 
         # Addition of numbers, splitting the answer into 2 -
@@ -41,12 +45,10 @@ class Solution(object):
             return ListNode(save_num, ListNode(remember_num, None))
         # l2 has a value, use it to carry the number.
         elif not l1:
-            l2.val = l2.val + remember_num
-            return ListNode(save_num, self.addTwoNumbers(l1, l2))
+            return ListNode(save_num, self.addTwoNumbers(ListNode(remember_num, None), l2))
         # l1 has a value, use it to carry the number.
         elif not l2:
-            l1.val = l1.val + remember_num
-            return ListNode(save_num, self.addTwoNumbers(l1, l2))
+            return ListNode(save_num, self.addTwoNumbers(l1, ListNode(remember_num, None)))
         # Both have a value, l1 has been selected to carry.
         else:
             l1.val = l1.val + remember_num
@@ -63,7 +65,8 @@ ln4 = ListNode(9, ListNode(9, ListNode(9, ListNode(9))))  # 999
 # 9,999,999 + 999 = 10,009,998
 
 s = Solution()
-test1 = s.addTwoNumbers(ln1, ln2)
+# test1 = s.addTwoNumbers(ln1, ln2)
+test1 = s.addTwoNumbers(ListNode(0, None), ListNode(0, None))
 test2 = s.addTwoNumbers(ln3, ln4)
 
 print()
