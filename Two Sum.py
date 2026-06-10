8c563e86dd09d6ee972d9b74aac59c64fdc95be9
ShaiOren4761 <shaioren100@gmail.com>
2026-06-10 13:39:56 +0300

    Two Sum was not the best way to unrust.

--- Two Sum.py
+++ Two Sum.py
@@ -7,12 +7,25 @@ def twoSum(nums, target):
     hashdict = {}  # init hash, key is number needed to complete the value.
 
     for i, val in enumerate(nums):
-        if hashdict.get(val) is not None: return [hashdict[val], i]
+        if hashdict.get(val) is not None:
+            return [hashdict[val], i]
 
-        else: hashdict.update({target - val: i})
+        else:
+            hashdict.update({target - val: i})
 
     return []
 
 
-print(twoSum([2, 7, 11, 15], 9))
+def twoSum_better(nums, target):
+    seen = {}  # init seen dict
 
+    for i, val in enumerate(nums):
+        complement = target - val
+
+        if complement in seen:
+            return [seen[target-val], i]
+        seen.update({val: i})
+
+    return []
+
+print(twoSum_better([2, 7, 11, 15], 9))
